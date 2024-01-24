5affe908c10babd1a3bf2fcc4bde0f4733cd160b
kiptoobrachok <calebkiptoo9090@gmail.com>
2024-01-24 23:28:23 +0300

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# puppet manifest to create a file with atrribute specifications
+
+file { '/tmp/school':
+    ensure  => file,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
