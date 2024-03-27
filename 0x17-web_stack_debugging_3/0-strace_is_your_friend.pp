e369341e1b8a6f002d839693b6c6f09e9eb259a9
KIPTOO CALEB <calebkiptoo9090@gmail.com>
2024-03-27 15:52:29 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-# puppet manuscript to replace line in a file on a server
-
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
-}
