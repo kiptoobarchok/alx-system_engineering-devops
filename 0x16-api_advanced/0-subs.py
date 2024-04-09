71de6af5485d8eb547c949ad48a731fecddad143
kiptoobrachok <calebkiptoo9090@gmail.com>
2024-04-09 17:58:17 +0300

    subreddit subscrbers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
 '''
 function to querry reddit API and return number of subscribers
 '''
 from requests import get
 
+
 def number_of_subscribers(subreddit):
-    if subreddit is None or not isinstance (subreddit, str):
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    
+
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
