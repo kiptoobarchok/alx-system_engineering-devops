9035893861505467a6446d916be0cc59f5f813fb
kiptoobarchok <calebkiptoo9090@gmail.com>
2024-06-19 21:11:57 +0000

    debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+# Puppet manifest to fix Apache 500 error
+#
+# Description: Fixing issue causing Apache 500 error
+#
+# Author: KIPTOO CALEB
+
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
